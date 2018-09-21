30124f1783da39574779d937f2700660781d2b5f
Liu Kaibo <liukaibo@Kaibo-MacBook-Pro.local>
2018-09-21 00:00:31 -0700

    new repo for remote work

---
+++ collect_data.sh
@@ -0,0 +1,5 @@
+scp -r kaibo@asimov-0-log.svail.baidu.com:/mnt/scratch/kaibo/proj/fordecoding_catchup/decode_result ./catchup
+scp -r kaibo@asimov-0-log.svail.baidu.com:/mnt/scratch/kaibo/proj/fordecoding/decode_result ./waitk
+scp  kaibo@asimov-0-log.svail.baidu.com:/mnt/data/mam/data/data_zh2en_2M/test_src.bpe ./testdata
+scp  kaibo@asimov-0-log.svail.baidu.com:/mnt/data/mam/data/data_zh2en_2M/test_src.text ./testdata
+scp -r  kaibo@asimov-0-log.svail.baidu.com:/mnt/data/mam/data/data_zh2en_2M/evaldata ./testdata
